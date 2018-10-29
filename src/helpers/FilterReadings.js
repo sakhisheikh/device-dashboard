fb9509427e1a29feae3ec380e6943f0312bcc2cc
sakhisheikh <sakhi.mansoor@venturedive.com>
2018-10-29 17:18:23 +0500

    Render Prop filter readings refactored

--- src/helpers/FilterReadings.js
+++ src/helpers/FilterReadings.js
@@ -5,41 +5,24 @@ import * as SearchUtils from '../utils/MatchSorter';
 
 class FilterReadings extends Component {
   state = {
-    inputValue: '',
     filterReadings: [],
-    updateState: false,
   };
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (
-      nextProps.inputValue !== prevState.inputValue ||
-      nextProps.updateState
-    ) {
-      return {
-        inputValue: nextProps.inputValue,
-        filterReadings: nextProps.readings,
-        updateState: nextProps.updateState,
-      };
-    }
-    return null;
-  }
-
   componentDidUpdate(prevProps) {
-    const { updateState } = this.state;
-    const { inputValue } = this.props;
-    if (prevProps.inputValue !== inputValue || updateState) {
-      const { readings, onUpdateReadingCount } = this.props;
+    const { inputValue, readings } = this.props;
+    if (
+      prevProps.inputValue !== inputValue ||
+      prevProps.readings !== readings
+    ) {
+      const { onUpdateReadingCount } = this.props;
       // Perform some operation here
       const filteredReadings = SearchUtils.MatchSorter({
         inputValue,
         readings,
       });
       onUpdateReadingCount(filteredReadings);
-      this.setState(state => {
-        return {
-          filterReadings: filteredReadings,
-          updateState: !state.updateState,
-        };
+      this.setState({
+        filterReadings: filteredReadings,
       });
     }
   }
